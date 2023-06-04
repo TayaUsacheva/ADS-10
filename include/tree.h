c2b2a3e4e8d60bb63ce4ac5cd5794a3a27408fde
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-06-04 22:32:33 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -20,7 +20,7 @@ class Tree {
             child->value = c;
             root->children.push_back(child);
             std::vector<char> remainingChars(vec);
-            remainingChars.erase(remainingChars.begin() + i);
+            remainingChars.erase(std::find(remainingChars.begin() + i), c);
             insert(child, remainingChars);
         }
     }
